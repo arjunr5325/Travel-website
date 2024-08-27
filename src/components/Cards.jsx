1e846d3699aef57d68e3b19a709fdb7aad8fbab8
arjunr5325 <arjunr5325aaa@gmail.com>
2024-08-28 00:34:57 +0530

    changed the links

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -33,13 +33,13 @@ function Cards() {
               src='images/img-4.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
               label='Adventure'
-              path='/products'
+              path='/product'
             />
             <CardItem
               src='images/img-8.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
               label='Adrenaline'
-              path='/sign-up'
+              path='/Signup'
             />
           </ul>
         </div>
